26cc22bb203a494d01c6ec3f461b7044e7d84a40
Alvaro Caicedo <caicedito03@gmail.com>
2020-11-21 03:06:32 -0500

    Mi primer conmit del proyecto repaso

---
+++ index.php
@@ -0,0 +1,11 @@
+<!Doctype html>
+<html>
+<head>
+    <title> Repaso Seminario </title>
+    <!-- Archivos estilos -->
+</head>
+<body>
+    <h1> Hola Mundo </h1>
+    <!-- Archivos Javascript -->
+</body>
+</html>
